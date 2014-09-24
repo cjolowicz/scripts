c420999fc7c28b7fcab701eaeee2b9c6a38ff441
Claudio Jolowicz <cj@dealloc.org>
2014-09-24 17:53:39 +0200

    [qreplay] Add.

---
+++ mercurial/qreplay.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+prog=$(basename $0)
+
+### usage ##############################################################
+
+usage() {
+    echo "\
+usage: $prog [options] [patch]..
+       $prog [options] --applied
+
+options:
+    -a, --applied    Replay all applied patches.
+        --cwd DIR    Change working directory.
+    -s, --sleep N    Sleep N seconds after each push.
+    -p, --print      Print each patch.
+    -h, --help       Display this message.
+"
+    exit
+}
+
+### command line #######################################################
+
+error() {
+    echo "$prog: error: $*" >&2
+    exit 1
+}
+
+bad_usage() {
+    echo "$prog: $*" >&2
+    echo "Try \`$prog --help' for more information." >&2
+    exit 1
+}
+
+missing_arg () {
+    bad_usage "option \`$1' requires an argument"
+}
+
+unknown_option () {
+    bad_usage "unrecognized option \`$1'"
+}
+
+sleep=
+cwd=
+applied=false
+print=false
+while [ $# -gt 0 ]
+do
+    option="$1"
+    shift
+
+    case $option in
+        --cwd) [ $# -gt 0 ] || missing_arg "$option" ; cwd="$1" ; shift ;;
+        -a | --applied) applied=true ;;
+        -p | --print) print=true ;;
+        -s | --sleep) [ $# -gt 0 ] || missing_arg "$option" ; sleep="$1" ; shift ;;
+        -h | --help) usage ;;
+        --) break ;;
+        -*) unknown_option "$option" ;;
+        *) set -- "$option" "$@" ; break ;;
+    esac
+done
+
+if [ -n "$cwd" ] ; then
+    cd "$cwd"
+fi
+
+if ! $applied ; then
+    [ $# -gt 0 ] || bad_usage "missing argument"
+else
+    [ $# -eq 0 ] || bad_usage "unexpected argument \`$1'"
+    set -- $(hg qapplied)
+fi
+
+### main ###############################################################
+
+qtop=$(hg qtop)
+
+for patch ; do
+    if [ "$(hg qtop)" != "$patch" ] ; then
+        hg qgoto --quiet "$patch" || exit $?
+    fi
+
+    ! $print || hg tip
+
+    [ -z "$sleep" ] || sleep $sleep
+done
+
+[ "$(hg qtop)" = "$qtop" ] || hg qgoto --quiet "$qtop"
