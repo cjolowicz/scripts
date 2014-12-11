adb60b6d5e783e1f0df60e421386286ee6c5a80f
Claudio Jolowicz <cj@dealloc.org>
2014-12-11 22:16:34 +0100

    [qfoldr] Add --dry-run. Shut up mq commands.

--- mercurial/qfoldr.sh
+++ mercurial/qfoldr.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 set -e
+set -o pipefail
 
 prog=$(basename $0)
 
-### usage ##############################################################
+### functions ##########################################################
 
-usage () {
+usage() {
     echo "usage: $prog [options]
 Fold the two top-most patches { A B } into B.
 
 options:
+    -n, --dry-run   Print commands instead of executing them.
     -h, --help      Display this message.
 
 This program performs a fold of two patches { A B } using the
@@ -30,13 +32,54 @@ metainformation, such as the filename and the description, is taken
 only from B."
 }
 
-### command line #######################################################
+error() {
+    echo "$prog: $*" >&2
+    exit 1
+}
 
-bad_option () {
+bad_option() {
     echo "$prog: unrecognized option \`$1'" >&2
     echo "Try \`$prog --help' for more information." >&2
     exit 1
 }
+
+for alias in qpush qpop qgoto ; do
+    unalias $alias 2>/dev/null || true
+done
+
+qpush() {
+    if $dry_run ; then
+        $run hg qpush --quiet "$@"
+    else
+        hg qpush --quiet "$@" 2>&1 | (
+            grep -Ev '^(now at:|patch .* is empty)' || true
+	) >&2
+    fi
+}
+
+qpop() {
+    if $dry_run ; then
+        $run hg qpop --quiet "$@"
+    else
+        hg qpop --quiet "$@" | (
+	    grep -v '^now at:' || true
+	)
+    fi
+}
+
+qgoto() {
+    if $dry_run ; then
+        $run hg qgoto --quiet "$@"
+    else
+        hg qgoto --quiet "$@" 2>&1 | (
+            grep -Ev '^(now at:|patch .* is empty)' || true
+	) >&2
+    fi
+}
+
+### command line #######################################################
+
+dry_run=false
 
 while [ $# -gt 0 ]
 do
@@ -44,6 +87,7 @@ do
     shift
 
     case $option in
+        -n | --dry-run) dry_run=true ;;
         -h | --help) usage ; exit ;;
         --) break ;;
         -*) bad_option $option ;;
@@ -53,14 +97,9 @@ done
 
 [ $# -eq 0 ] || bad_option "$1"
 
-### functions ##########################################################
-
-hgroot="$(hg root)"
-
-error() {
-    echo "$prog: $*" >&2
-    exit 1
-}
+if $dry_run ; then
+    run=echo
+fi
 
 ### main ###############################################################
 
@@ -68,10 +107,10 @@ aname=$(hg qprev)
 bname=$(hg qtop)
 cname=TMP-$bname
 
-hg qrefresh -X .
-hg qnew $cname
-hg qgoto $aname
-hg qrefresh -m ''
-hg qpop
-hg qpush --move $bname
-hg qfold $aname $cname
+$run hg qrefresh --exclude .
+$run hg qnew $cname
+qgoto $aname
+$run hg qrefresh --message= # FIXME: this has no effect
+qpop
+qpush --move $bname
+$run hg qfold $aname $cname
