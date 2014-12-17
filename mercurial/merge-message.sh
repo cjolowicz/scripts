eb632c51bd29cf748d9163abd17a4d103b428969
Claudio Jolowicz <cj@dealloc.org>
2014-12-17 20:25:06 +0100

    [merge-message] Add --output. Improve usage message.

--- mercurial/merge-message.sh
+++ mercurial/merge-message.sh
@@ -7,10 +7,23 @@ prog=$(basename $0)
 usage () {
     echo "usage: $prog [options] [REV]
 
-options:
-    -h, --help    Display this message.
-
 Print the commit message for \`hg merge REV'.
+
+options:
+    -o, --output FILE       Write output to the specified file.
+    -R, --repository REPO   Repository root directory or name of overlay bundle
+                            file.
+        --cwd DIR           Change working directory.
+        --config CONFIG [+] Set/override config option. (use 'section.name=value')
+        --encoding ENCODE   Set the charset encoding. (default: UTF-8)
+        --encodingmode MODE Set the charset encoding mode. (default: strict)
+        --color TYPE        When to colorize. (boolean, always, auto, or never)
+                            (default: auto)
+        --pager TYPE        When to paginate. (boolean, always, auto, or never)
+                            (default: auto)
+    -h, --help              Display this message.
+
+[+] marked option can be specified multiple times
 "
     exit
 }
@@ -28,7 +41,8 @@ bad_usage () {
 
 ### command line #######################################################
 
-hg_options=()
+options=()
+output=/dev/stdout
 
 while [ $# -gt 0 ]
 do
@@ -36,6 +50,12 @@ do
     shift
 
     case $option in
+        -o | --output)
+            [ $# -gt 0 ] || bad_usage "option \`$option' requires an argument"
+            output="$1"
+            shift
+            ;;
+
         -R | --repository    | \
              --cwd           | \
              --config        | \
@@ -44,7 +64,7 @@ do
              --color         | \
              --pager)
             [ $# -gt 0 ] || bad_usage "option \`$option' requires an argument"
-            hg_options+=("$option" "$1")
+            options+=("$option" "$1")
             shift
             ;;
 
@@ -62,10 +82,10 @@ done
 if [ $# -eq 0 ] ; then
     echo "Merge."
     echo
-    merge-preview "${hg_options[@]}" --template ' * {desc|firstline} [{node|short}]\n'
+    merge-preview "${options[@]}" --template ' * {desc|firstline} [{node|short}]\n'
 else
     echo "Merge $1 branch."
     echo
-    merge-preview "${hg_options[@]}" --template ' * [{branch}/{node|short}] {desc|firstline}\n' "$1" |
+    merge-preview "${options[@]}" --template ' * [{branch}/{node|short}] {desc|firstline}\n' "$1" |
     sed "s,\\[$1/,[,"
-fi
+fi > "$output"
