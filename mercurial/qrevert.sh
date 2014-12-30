e0a05c712c6301d8f1e155ff39271f50efda78a3
Claudio Jolowicz <cj@dealloc.org>
2014-12-30 20:16:31 +0100

    [qrevert] Add.

--- mercurial/qrevert.sh
+++ mercurial/qrevert.sh
@@ -10,9 +10,15 @@ prog=$(basename $0)
 usage() {
     echo "\
 usage: $prog [options]
-Restore queue to the previous saved state.
+Restore queue to its checkout state.
+
+This program functions as a front-end to \`hg revert --mq'. Patches
+are popped before reverting the patch queue. The program attempts to
+re-apply the previous patches after reverting.
 
 options:
+    -r, --rev REV   Revert to the specified queue revision.
+    -C, --no-backup Do not save backup copies of patches.
         --cwd DIR   Change working directory.
     -n, --dry-run   Print commands instead of executing them.
     -h, --help      Display this message."
@@ -38,6 +44,7 @@ missing_arg() {
 
 cwd=
 dry_run=false
+options=()
 while [ $# -gt 0 ]
 do
     option="$1"
@@ -48,6 +55,16 @@ do
             [ $# -gt 0 ] || missing_arg "$option"
             cwd="$1"
             shift
+            ;;
+
+        -r | --rev)
+            [ $# -gt 0 ] || missing_arg "$option"
+            options+=(--rev "$1")
+            shift
+            ;;
+
+        -C | --no-backup)
+            options+=(--no-backup)
             ;;
 
         -n | --dry-run) dry_run=true ;;
@@ -75,10 +92,10 @@ fi
 hg root >/dev/null || error "no repository"
 hg root --mq >/dev/null || error "no patch repository"
 
-oldtop="$(hg qtop)" 2>/dev/null
+oldtop="$(hg qtop | grep -v 'no patches applied')" 2>/dev/null || true
 
-$run hg qpop --all
-$run hg update --clean --mq
-$run hg purge --mq
+[ -z "$oldtop" ] || $run hg qpop --all
+
+$run hg revert --mq --all "${options[@]}"
 
 [ -z "$oldtop" ] || $run hg qgoto "$oldtop"
