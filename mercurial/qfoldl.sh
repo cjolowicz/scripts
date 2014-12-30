02227251a7adf75d7d93825f967d952629e87ec8
Claudio Jolowicz <cj@dealloc.org>
2014-12-30 21:41:59 +0100

    [qfoldl] Make verbosity levels distinguishable in the output.

--- mercurial/qfoldl.sh
+++ mercurial/qfoldl.sh
@@ -44,7 +44,11 @@ verbose() {
     local level=$1
     shift
 
-    [ $verbose -lt $level ] || echo "$prog: $*" >&2
+    if [ $verbose -gt $level ] ; then
+        echo "$prog: *** $*" >&2
+    elif [ $verbose -eq $level ] ; then
+        echo "$prog: $*" >&2
+    fi
 }
 
 for alias in qpop qrefresh qfold ; do
