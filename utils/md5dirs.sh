9f02d26a79f6b4ef996752c16af4985bfd6e203a
Claudio Jolowicz <cj@dealloc.org>
2017-02-10 17:55:22 +0100

    [md5dirs] Handle file names with spaces.

--- utils/md5dirs.sh
+++ utils/md5dirs.sh
@@ -9,8 +9,8 @@ trap 'rm -rf $tmpdir' 0
 list1="$tmpdir"/1
 list2="$tmpdir"/2
 
-find "$dir1" -type f | xargs -n1 md5sum | sort | sed "s,${1//,\\,},a,g" > "$list1"
-find "$dir2" -type f | xargs -n1 md5sum | sort | sed "s,${2//,\\,},b,g" > "$list2"
+find "$dir1" -type f -print0 | xargs -0 -n1 md5sum | sort | sed "s,${1//,\\,},a,g" > "$list1"
+find "$dir2" -type f -print0 | xargs -0 -n1 md5sum | sort | sed "s,${2//,\\,},b,g" > "$list2"
 
 echo "Identical Files:"
 echo
