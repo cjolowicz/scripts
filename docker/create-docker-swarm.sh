bd345dca89d3b8e87eeb6e09bdf172a2f3000ce2
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-03-30 19:11:23 +0100

    [create-docker-swarm] Fixes

--- docker/create-docker-swarm.sh
+++ docker/create-docker-swarm.sh
@@ -1,40 +1,35 @@
 #!/bin/bash
-# Based on: http://mmorejon.github.io/en/blog/docker-swarm-with-docker-machine-scripts/
 
-set -euo pipefail
+set -exuo pipefail
 
 nmanagers=1
 nworkers=3
 
-leader=$leader
+leader=node1
 managers=node{1..$nmanagers}
 workers=node{$((nmanagers + 1))..$((nmanagers + nworkers))}
 
-echo "### Creating nodes ..."
 for node in ${managers[@]} ${workers[@]}
+do
     docker-machine create --driver=virtualbox $node
 done
 
 leader_ip=$(docker-machine ip $leader)
 
-echo "### Initializing Swarm ..."
 eval $(docker-machine env $leader)
-docker swarm init --advertise-addr $leader_ip
+docker swarm init --advertise-addr=$leader_ip
 
 manager_token=$(docker swarm join-token --quiet manager)
 worker_token=$(docker swarm join-token --quiet worker)
 
-echo "### Joining managers ..."
-for node in ${managers[@]} ; do
-    if [ $node != $leader ]
-    then
-        eval $(docker-machine env $node)
-        docker swarm join --token $manager_token $leader_ip:2377
-    fi
+for node in ${managers[@]:1}
+do
+    eval $(docker-machine env $node)
+    docker swarm join --token=$manager_token $leader_ip:2377
 done
 
-echo "### Joining workers ..."
-for node in ${workers[@]} ; do
+for node in ${workers[@]}
+do
     eval $(docker-machine env $node)
-    docker swarm join --token $worker_token $leader_ip:2377
+    docker swarm join --token=$worker_token $leader_ip:2377
 done
