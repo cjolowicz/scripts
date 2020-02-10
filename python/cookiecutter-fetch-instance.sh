7c680f00f6316da7cba1726b5ee7d1db885be2d2
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-02-10 06:29:00 +0100

    [cookiecutter-fetch-instance] Add

---
+++ python/cookiecutter-fetch-instance.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+set -euo pipefail
+
+program=$(basename $0)
+usage="\
+usage: $program --init [URL]
+       $program fetch
+
+Dependencies:
+
+   pipx install git-filter-repo
+"
+github_user=$USER
+cookiecutter_project=$(pwd | xargs basename)
+cookiecutter_package=${cookiecutter_project//-/_}
+cookiecutter_url=$(git remote get-url origin)
+project_name=${cookiecutter_project}-instance
+package_name=${cookiecutter_package}_instance
+replace_text=$(mktemp)
+
+trap 'rm -f $replace_text' 0
+
+cat <<EOF > $replace_text
+$project_name==>{{cookiecutter.project_name}}
+$package_name==>{{cookiecutter.package_name}}
+EOF
+
+message_callback='
+import re
+
+if not message:
+    return message
+
+pattern = re.compile(b" [(]#([0-9]+)[)]$")
+replacement = b"\n\n'"$github_user/$project_name"'#\\1"
+
+lines = message.splitlines(keepends=True)
+
+if not pattern.search(lines[0]):
+    return message
+
+head = pattern.sub(replacement, lines[0])
+body = b"".join(lines[1:])
+
+return head + body
+'
+
+filter_options=(
+    --force
+    --refs=instance
+    --to-subdirectory-filter='{{cookiecutter.project_name}}'
+    --path-rename="$project_name:{{cookiecutter.project_name}}"
+    --path-rename="$package_name:{{cookiecutter.package_name}}"
+    --replace-text="$replace_text"
+    --message-callback="$message_callback"
+)
+
+init() {
+    if [ $# -eq 0 ]
+    then
+        url=${cookiecutter_url%.git}-instance.git
+    else
+        url="$1"
+    fi
+
+    git remote add instance $url
+}
+
+fetch() {
+    git fetch instance
+    git checkout -B instance instance/master
+    git filter-repo "${filter_options[@]}"
+}
+
+while [ $# - gt 0 ]
+do
+    option="$1"
+    shift
+
+    case $option in
+        --init)
+            init "$@"
+            exit
+            ;;
+
+        --help | -h)
+            echo "$usage"
+            exit
+            ;;
+
+        *)
+            echo "$usage"
+            exit 1
+            ;;
+    esac
+done
+
+fetch
