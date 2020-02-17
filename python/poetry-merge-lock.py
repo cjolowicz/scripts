ed1da5785e4d5fc2573b875deef8ffe77070c1ea
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-02-17 11:03:47 +0100

    Use hybrid merge strategy at TOML and application level

--- python/poetry-merge-lock.py
+++ python/poetry-merge-lock.py
@@ -3,10 +3,11 @@ import sys
 import os
 
 sys.path.insert(0, os.path.expanduser("~/.poetry/lib"))  # noqa
+from collections import defaultdict
 from enum import Enum
 import itertools
 import os
-from typing import Iterator, List, Optional, Sequence, Tuple
+from typing import Any, Dict, Iterator, List, Optional, Sequence, Tuple
 
 from poetry.factory import Factory
 from poetry.packages import Package
@@ -146,25 +147,71 @@ def load_versions(toml_file: Path) -> Tuple[TOMLDocument, TOMLDocument]:
         return load(ours), load(theirs)
 
 
+class MergeConflictError(ValueError):
+    """
+    An item in the TOML document cannot be merged.
+    """
+
+    def __init__(self, keys: List[tomlkit.api.Key], ours: Any, theirs: Any):
+        message = "Merge conflict at {}, merging {!r} and {!r}".format(
+            ".".join(str(key) for key in keys), ours, theirs
+        )
+        super().__init__(message)
+
+
+AnyDict = Dict[str, Any]
+AnyDictList = List[AnyDict]
+
+
+def merge_packages(ours: AnyDictList, theirs: AnyDictList) -> AnyDictList:
+    packages = {}
+    for package in itertools.chain(ours, theirs):
+        value = packages.setdefault(package["name"], package)
+        if package.value != value.value:
+            raise MergeConflictError(["package"], value, package)
+
+    return list(packages.values())
+
+
+def merge_files(ours: AnyDict, theirs: AnyDict) -> AnyDict:
+    files = tomlkit.table()
+    for key in set(itertools.chain(ours, theirs)):
+        our_value = ours.get(key)
+        their_value = theirs.get(key)
+        if None not in (our_value, their_value) and our_value != their_value:
+            raise MergeConflictError(["metadata", "files", key], our_value, their_value)
+        files[key] = our_value if our_value is not None else their_value
+    return files
+
+
+def merge_versions(ours: TOMLDocument, theirs: TOMLDocument) -> TOMLDocument:
+    document = tomlkit.document()
+    document["package"] = merge_packages(ours["package"], theirs["package"])
+    document["metadata"] = {
+        "files": merge_files(ours["metadata"]["files"], theirs["metadata"]["files"]),
+    }
+    return document
+
+
+def activate_dependencies(packages: List[Package]) -> None:
+    for package in packages:
+        for dependency in package.requires:
+            if dependency.is_optional():
+                dependency.activate()
+
+
 def load_packages(locker: Locker, lock_data: TOMLDocument) -> List[Package]:
     locker._lock_data = lock_data
     repository = locker.locked_repository(with_dev_reqs=True)
+    activate_dependencies(repository.packages)
     return repository.packages
-
-
-def merge_packages(ours: List[Package], theirs: List[Package]) -> List[Package]:
-    packages = set(itertools.chain(ours, theirs))
-    # TODO: same package, different version (e.g. upgrades on both branches)
-    return list(packages)
 
 
 def merge_locked_packages(locker: Locker) -> List[Package]:
     lock_file = Path(locker.lock._path)
-    ours, theirs = [
-        load_packages(locker, lock_data)
-        for lock_data in load_versions(lock_file)
-    ]
-    return merge_packages(ours, theirs)
+    ours, theirs = load_versions(lock_file)
+    lock_data = merge_versions(ours, theirs)
+    return load_packages(locker, lock_data)
 
 
 def main() -> None:
