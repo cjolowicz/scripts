52fa9379475902057c99b8fc76d262890e9d7fe7
Claudio Jolowicz <mail@claudiojolowicz.com>
2020-11-25 15:57:48 +0100

    [github-fork] Handle URLs; support existing forks; support own repositories

--- github/github-fork.sh
+++ github/github-fork.sh
@@ -5,10 +5,39 @@ set -euo pipefail
 repo=$1
 shift
 
-repodir=$HOME/Code/github.com/$repo
+prefixes=(
+    https://github.com/
+    git@github.com:
+)
+
+for prefix in ${prefixes[@]}
+do
+    case $repo in
+        ${prefix}*)
+            repo=${repo#$prefix}
+            break
+            ;;
+    esac
+done
+
+owner=${repo%%/*}
+repo=${repo#*/}
+
+repodir=$HOME/Code/github.com/$owner/$repo
 basedir=$(dirname $repodir)
 
 mkdir -p $basedir
 cd $basedir
-gh repo fork --clone=true $repo
+
+if [ "$owner" = "$USER" ]
+then
+    git clone git@github.com:$owner/$repo
+elif curl --head --fail --silent https://github.com/$USER/$repo >/dev/null
+then
+    git clone git@github.com:$USER/$repo
+    git -C $repo remote add upstream git@github.com:$owner/$repo
+else
+    gh repo fork --clone=true $owner/$repo
+fi
+
 echo $repodir
