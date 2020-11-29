7369e7a50b9fce03197bf8cbeef55cae4a2c9d54
Claudio Jolowicz <mail@claudiojolowicz.com>
2020-11-29 23:18:09 +0100

    [phonotactic] Distinguish between onset, nucleus, and coda

--- linguistics/phonotactic.py
+++ linguistics/phonotactic.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
+from __future__ import annotations
+
 import collections
+import dataclasses
 import sys
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Iterable
 from typing import Iterator
+from typing import List
 from typing import TextIO
 from typing import Tuple
 
@@ -19,55 +24,100 @@ SEMIVOWELS = "Y"
 EXCLUDE = "ÂÊÉ"
 
 
-def phonotactic_split(word: str) -> Iterator[str]:
-    previous = -1
-    for index, character in enumerate(word):
-        if character in SEMIVOWELS:
-            current = word[previous + 1 : index]
-            if current:
-                yield current
+@dataclass
+class Syllable:
+    onset: str = ""
+    nucleus: str = ""
+    coda: str = ""
+
+    @classmethod
+    def parse_onset(cls, text: str) -> Iterator[str]:
+        for index, character in enumerate(text):
+            if character in VOWELS:
+                return
+
+            if character in SEMIVOWELS:
+                if index > 0 or text[index + 1 : index + 2] not in VOWELS:
+                    return
+
             yield character
-            previous = index
-        elif index > 0 and (
-            (character in VOWELS) != (word[index - 1] in VOWELS)
-        ):
-            current = word[previous + 1 : index]
-            if current:
-                yield current
-            previous = index
-    current = word[previous + 1 :]
-    if current:
-        yield current
+
+    @classmethod
+    def parse_nucleus(cls, text: str) -> Iterator[str]:
+        for character in text:
+            if character not in VOWELS + SEMIVOWELS:
+                return
+
+            yield character
+
+    @classmethod
+    def parse_coda(cls, text: str) -> Iterator[str]:
+        for character in text:
+            if character in VOWELS + SEMIVOWELS:
+                return
+
+            yield character
+
+    @classmethod
+    def parse(cls, text: str) -> Syllable:
+        rest = text
+        onset = "".join(cls.parse_onset(rest))
+        rest = rest[len(onset) :]
+        nucleus = "".join(cls.parse_nucleus(rest))
+        rest = rest[len(nucleus) :]
+        coda = "".join(cls.parse_coda(rest))
+        rest = rest[len(coda) :]
+        if rest:
+            raise ValueError(f"{text}: vowels after coda: {rest}")
+        return cls(onset, nucleus, coda)
 
 
-def syllabic_split(hyphenate: pyphen.Pyphen, word: str) -> Iterator[str]:
+def syllabic_split(hyphenate: pyphen.Pyphen, word: str) -> List[Syllable]:
     positions = hyphenate.positions(word)
     positions = [0, *positions, len(word)]
-    for start, end in zip(positions, positions[1:]):
-        yield word[start:end]
+    return [
+        Syllable.parse(word[start:end]) for start, end in zip(positions, positions[1:])
+    ]
 
 
-def phonotactic_clusters(words: Iterable[str]) -> Iterable[Tuple[str, int]]:
-    counter = collections.Counter()
+@dataclass
+class Histogram:
+    onset: collections.Counter = dataclasses.field(default_factory=collections.Counter)
+    nucleus: collections.Counter = dataclasses.field(default_factory=collections.Counter)
+    coda: collections.Counter = dataclasses.field(default_factory=collections.Counter)
+
+    def update(self, syllable: Syllable) -> None:
+        self.onset[syllable.onset] += 1
+        self.nucleus[syllable.nucleus] += 1
+        self.coda[syllable.coda] += 1
+
+
+def phonotactic_clusters(words: Iterable[str]) -> Histogram:
+    histogram = Histogram()
     hyphenate = pyphen.Pyphen(lang="de")
     for word in words:
         word = word.upper()
         if any(character in word for character in EXCLUDE):
             continue
-        for syllable in syllabic_split(hyphenate, word):
-            for cluster in phonotactic_split(syllable):
-                counter[cluster] += 1
-    return counter.most_common()
+        try:
+            for syllable in syllabic_split(hyphenate, word):
+                histogram.update(syllable)
+        except ValueError as error:
+            pass  #print(f"{word}: {error}", file=sys.stderr)
+    return histogram
 
 
-def print_phonotactic_clusters(io: TextIO):
+def print_phonotactic_clusters(io: TextIO) -> None:
     words = tqdm.tqdm(io.read().splitlines())
-    for syllable, count in phonotactic_clusters(words):
-        if count > 1:
-            print(f"{count:-10d} {syllable}")
+    histogram = phonotactic_clusters(words)
+    for attr in ("onset", "nucleus", "coda"):
+        print(f"==> {attr} <==")
+        for syllable, count in getattr(histogram, attr).most_common():
+            if syllable and count > 1:
+                print(f"{count:-10d} {syllable}")
 
 
-def main():
+def main() -> None:
     if sys.argv[1:]:
         with open(sys.argv[1]) as io:
             print_phonotactic_clusters(io)
