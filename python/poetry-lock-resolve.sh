08b9c52e08266a3e121ca7fb50fd56145eb63fee
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-06 13:13:07 +0200

    [poetry-lock-resolve] Add option --rebase

--- python/poetry-lock-resolve.sh
+++ python/poetry-lock-resolve.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+set -euo pipefail
+
+rebase=false
+
+if [ $# -gt 0 ]
+then
+    case $1 in
+        --rebase)
+            rebase=true
+            ;;
+    esac
+fi
+
+if $rebase
+then
+    while :
+    do
+        if $0 && GIT_EDITOR=: git rebase --continue
+        then
+            break
+        fi
+    done
+    exit
+fi
+
 git restore --worktree --staged poetry.lock
 poetry lock --no-update
 git add poetry.lock
