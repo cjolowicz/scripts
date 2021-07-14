4a71942d255af0e9b2eefd64cdfb6691d107eae4
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-14 23:04:35 +0200

    [mypwgen] Use XKCD algorithm by default, --bytes for old behavior

--- admin/mypwgen.sh
+++ admin/mypwgen.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
-dd if=/dev/urandom count=1 2>/dev/null | tr -d -c '[:graph:]' | cut -c-${1:-8}
+
+case $1 in
+    '--bytes')
+        shift
+        bytes=true
+        ;;
+
+    *)
+        bytes=false
+        ;;
+esac
+
+if [ $# -gt 0 ]
+then
+    count=$1
+    shift
+fi
+
+if $bytes
+then
+    dd if=/dev/urandom count=1 2>/dev/null | tr -d -c '[:graph:]' | cut -c-${count:-8}
+else
+    shuf --head-count=${count:-4} --random-source=/dev/urandom /usr/share/dict/words | xargs
+fi
