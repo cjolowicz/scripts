46771ee09b2699978bb3470c1208f5e9fe452fdf
Claudio Jolowicz <mail@claudiojolowicz.com>
2023-06-26 22:18:57 +0200

    [update] Add

---
+++ admin/update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+
+function header() {
+    echo
+    echo "==> $* <=="
+    echo
+}
+
+header "Homebrew"
+brew update && brew upgrade
+
+header "Spacemacs"
+git -C ~/.emacs.d fetch
+
+if ! git -C ~/.emacs.d diff --quiet develop origin/develop
+then
+    git -C ~/.emacs.d plog -p --stat --reverse develop..origin/develop
+    git -C ~/.emacs.d pull
+else
+    echo "Already up-to-date."
+fi
