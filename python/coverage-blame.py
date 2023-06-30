a23ba68191dbf54e0ec1a0ef2cc392a4a34dde0b
Claudio Jolowicz <mail@claudiojolowicz.com>
2023-06-30 23:38:40 +0200

    [coverage-blame] add

---
+++ python/coverage-blame.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import json
+import os
+import shutil
+import subprocess
+from pathlib import Path
+
+
+def main() -> None:
+    tmpdir = Path("/tmp/coverage-blame")
+    shutil.rmtree(tmpdir, ignore_errors=True)
+    adir, bdir = tmpdir / "a", tmpdir / "b"
+    adir.mkdir(parents=True, exist_ok=True)
+    bdir.mkdir(parents=True, exist_ok=True)
+
+    process = subprocess.run(
+        ["pipx", "run", "coverage", "json", "--fail-under=0", "-o-"],
+        check=True,
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+    data = json.loads(process.stdout)
+    for filename, result in data["files"].items():
+        with Path(filename).open() as io, (adir / filename).open(mode="w") as a, (
+            bdir / filename
+        ).open(mode="w") as b:
+            missing = set(result["missing_lines"]) | {
+                a for a, b in result["missing_branches"]
+            }
+            for lineno, line in enumerate(io, start=1):
+                a.write(line)
+                if lineno not in missing:
+                    b.write(line)
+    subprocess.run(
+        [
+            "delta",
+            "--line-numbers",
+            "--line-numbers-right-format=",
+            str(adir),
+            str(bdir),
+        ]
+    )
+
+
+if __name__ == "__main__":
+    main()
