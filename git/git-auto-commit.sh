31c61e7dad8d17e0070b080abee68f4f505da211
Claudio Jolowicz <mail@claudiojolowicz.com>
2023-07-03 15:04:56 +0200

    [git-auto-commit] support added files

--- git/git-auto-commit.sh
+++ git/git-auto-commit.sh
@@ -2,14 +2,14 @@
 
 set -euo pipefail
 
-script='
-set -euo pipefail
+if [ $# -eq 0 ]
+then
+    # Use xargs -n1 to avoid buffering.
+    exec fswatch -r0 . | xargs -n1 -0 git ls-files -z | xargs -n1 -0 "$0"
+fi
 
 for file
 do
     message=$(realpath --relative-to=. "$file")
     git commit --message="$message" "$file"
 done
-'
-
-git ls-files -z | xargs -0 fswatch -0 | xargs -0 -n1 bash -c "$script" "$0"
