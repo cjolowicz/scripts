5211037e430ed5572345faf09a87146bdde4a38c
Claudio Jolowicz <claudio@cloudflare.com>
2023-12-04 17:56:47 +0100

    [update] Update scripts and dotfiles

--- admin/update.sh
+++ admin/update.sh
@@ -8,6 +8,50 @@ function header() {
     echo
 }
 
+function git-update() {
+    local directory="$1"
+    shift
+
+    [ $# -gt 0 ] || set -- main
+
+    local branch="$1"
+    shift
+
+    (
+        cd "$directory"
+
+        local stash="${RANDOM}"
+
+        if [ -n "$(git status --porcelain)" ]
+        then
+            git stash push --message="$stash"
+        fi
+
+        git fetch --prune --tags
+
+        if git diff --quiet "$branch" origin/"$branch"
+        then
+            echo "Already up-to-date."
+        else
+            git log --patch --stat --reverse "$branch"..origin/"$branch"
+            git pull --rebase
+        fi
+
+        if git stash list | grep --quiet "$stash"
+        then
+            git stash pop --index
+            # Don't specify `$stash` to avoid this error:
+            # fatal: log for 'refs/stash' only has 1 entries
+        fi
+    )
+}
+
+header "cjolowicz/dotfiles"
+git-update ~/Code/github.com/cjolowicz/dotfiles
+
+header "cjolowicz/scripts"
+git-update ~/Code/github.com/cjolowicz/scripts master
+
 header "Homebrew"
 brew update && brew upgrade
 
@@ -17,13 +61,5 @@ pipx upgrade-all --include-injected
 header "pip"
 py -m pip install --user --upgrade pip debugpy
 
-header "Spacemacs"
-git -C ~/.emacs.d fetch
-
-if ! git -C ~/.emacs.d diff --quiet develop origin/develop
-then
-    git -C ~/.emacs.d plog -p --stat --reverse develop..origin/develop
-    git -C ~/.emacs.d pull
-else
-    echo "Already up-to-date."
-fi
+header "Emacs"
+git-update ~/.emacs.d develop
