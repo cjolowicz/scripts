e38c837b9ce9c78cecc0235652e1a76f196b58eb
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-10-21 17:05:58 +0200

    [xml2json] add

---
+++ python/xml2json.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env uv run
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "xmlschema",
+# ]
+# ///
+import argparse
+import decimal
+import json
+
+import xmlschema
+
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return str(o)
+        return super().default(o)
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file")
+    parser.add_argument("--schema")
+    args = parser.parse_args()
+
+    if args.schema:
+        schema = xmlschema.XMLSchema(args.schema)
+        data = schema.to_dict(args.file)
+    else:
+        data = xmlschema.to_dict(args.file)
+
+    print(json.dumps(data, cls=DecimalEncoder))
+
+
+if __name__ == "__main__":
+    main()
