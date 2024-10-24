760e05f4ccd191dbc874196e0dac3012622efdb5
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-10-24 09:12:55 +0200

    [xml] add syntax highlighting

--- python/xml.sh
+++ python/xml.sh
@@ -1,6 +1,12 @@
-#!/usr/bin/env python3
+#!/bin/bash
 
+set -euo pipefail
+
+python3 -IPc <<EOF | bat -pl xml
 import sys
 import xml.dom.minidom
 
 print(xml.dom.minidom.parseString(sys.stdin.read()).toprettyxml())
+EOF
+
+
